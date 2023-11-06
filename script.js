0cf436eea5de75bff02e67ba3290191039c41173
Thyago <thiagogg020676@gmail.com>
2023-11-06 20:18:44 -0300

    Endireitando ofertas inicias

--- script.js
+++ script.js
@@ -1,41 +1,22 @@
 'use strict'
 
-const prodMain = [
-  { id: '1', url: "./Main/Main3/produto1.jpeg"},
-  { id: '2', url: "./Main/Main3/produto2.jpeg"},
-  { id: '3', url: "./Main/Main3/produto3.jpeg"},
-  { id: '4', url: "./Main/Main3/produto4.jpeg"},
-  { id: '5', url: "./Main/Main3/produto5.jpeg"},
-  { id: '6', url: "./Main/Main3/produto6.jpeg"},
-  { id: '7', url: "./Main/Main3/produto7.jpeg"},
-]
+let prev = document.getElementById('next')
+let nex = document.getElementById('prev')
 
-const container = document.querySelector('.slide-images')
+let containerItems = document.querySelector('.img-ofertas')
+console.log(containerItems)
 
-const loadImages = function (prodMain, container) {
-  prodMain.forEach(image => {
-    container.innerHTML += `<div class='item'>
-      <img src='${image.url}'>
-    </div>`
-  })
+prev.addEventListener('click', previous)
+nex.addEventListener('click', next)
+
+function previous(){
+  let ofertas = document.querySelectorAll('.img-ofertas img');
+  containerItems.appendChild(ofertas[0]);
 }
 
-loadImages(prodMain, container)
-
-let items = document.querySelectorAll('.item')
-
-const previous = () => {
-  container.appendChild(items[0])
-  items = document.querySelectorAll('.item')
+function next(){
+  let ofertas = document.querySelectorAll('.img-ofertas img');
+  const lastItem = ofertas[ofertas.length - 1];
+  containerItems.insertBefore( lastItem, ofertas[0] );
 }
 
-document.querySelector('#button1').addEventListener('click', previous)
-
-const next = () => {
-  const lastItem = items[items.length - 1]
-
-  container.insertBefore(lastItem, items[0])
-  items = document.querySelectorAll('.item')
-}
-
-document.querySelector('#button2').addEventListener('click', next)
